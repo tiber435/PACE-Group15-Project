b947ad78ade8b249169612930a6f9b1be000e980
liamtcrowe <liamtcrowe@gmail.com>
2024-04-17 16:39:00 +1000

    Installed Transformers
    Used same process on local machine as I have here, only got up to installing PyTorch. Had issues with the Python file used for reading files.

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\1kath\Documents\PACE-Group15-Project\.env
+set VIRTUAL_ENV=C:\Users\liamt\Documents\GitHub\PACE-Group15-Project\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
